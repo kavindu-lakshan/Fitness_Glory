9d9204d604e9e687279ec1a1e084708c50b7c833
Kavindu Lakshan <kavindulakshan26@gmail.com>
2021-08-04 18:53:38 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require ('dotenv');
+const app = express();
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+}));
+app.use(bodyParser.json());
+
+app.get("/test", (req, res) => {
+    res.send("It works"); 
+});
+
+app.use(express.json());
+app.use(cookieParser());
+ 
+//connect to mongoDB
+const URL= process.env.MONGODB_URL;
+
+mongoose.connect(URL,{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+})
+const connection =mongoose.connection;
+connection.once("open", ()=>{
+    console.log("connection success")
+})
+
+
+//Lakshan
+
+//K shehan
+
+//B Shehan
+
+//Dulshan
+
+//Amantha
+
+//Janudi
+
+//Manushi
+
+//Sandani
+
+app.listen(PORT,()=>{
+    console.log(`Server running on PORT: ${PORT}`)
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
