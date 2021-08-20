87a33b322672ad8a01703761d0ac0164e107bac8
Kavindu Lakshan <kavindulakshan26@gmail.com>
2021-08-20 22:26:39 +0530

    User Login, Register and Update user account

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-import axios from 'axios'
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom'
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/footer";
+import LandingPage from "./Screens/LandingPage/LandingPage";
+import HomePage from "./Screens/HomePage/HomePage";
+import { BrowserRouter, Route } from "react-router-dom";
+import LoginScreen from "./Screens/LoginScreen/LoginScreen";
+import RegisterScreen from "./Screens/RegisterScreen/RegisterScreen";
+import ProfileScreen from "./Screens/ProfileScreen/ProfileScreen";
 
-export default function App() {
+const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Route path="/" component={LandingPage} exact />
+        <Route path="/login" component={LoginScreen} />
+        <Route path="/profile" component={ProfileScreen} />
+        <Route path="/register" component={RegisterScreen} />
+        <Route path="/Home" component={() => <HomePage />} />
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+export default App;
