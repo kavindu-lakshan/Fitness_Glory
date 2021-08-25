3931fcef2991860df8a69c107bc0a95d3b21cf2b
Kavindu Shehan <86120540+kavindushehan@users.noreply.github.com>
2021-08-25 10:36:44 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,6 @@ const connectDB = async () => {
   try {
     //Shehan's testing database. just ignore this.
     //'mongodb+srv://Shehanx86:test123@cluster0.prwte.mongodb.net/fitness_glory_shehan?retryWrites=true&w=majority'
-
     const con = await mongoose.connect(process.env.MONGODB_URL, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
