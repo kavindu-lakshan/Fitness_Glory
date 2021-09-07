7c996056447a50120f1cca333f44c65e968843e2
Shehan Bartholomeusz <it20274702@my.sliit.lk>
2021-09-07 21:55:35 +0530

    member all programs1

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,7 @@ const connectDB = async () => {
   try {
     //Shehan's testing database. just ignore this.
     //'mongodb+srv://Shehanx86:test123@cluster0.prwte.mongodb.net/fitness_glory_shehan?retryWrites=true&w=majority'
-    //process.env.MONGODB_URL
-    const con = await mongoose.connect('mongodb+srv://Shehanx86:test123@cluster0.prwte.mongodb.net/fitness_glory_shehan1?retryWrites=true&w=majority', {
+    const con = await mongoose.connect(process.env.MONGODB_URL, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
