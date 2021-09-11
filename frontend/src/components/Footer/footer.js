bd3692246b50eb5f6636f3fd80bb8001ed225a39
Shehan Bartholomeusz <it20274702@my.sliit.lk>
2021-09-11 18:57:10 +0530

    footer

--- frontend/src/components/Footer/footer.js
+++ frontend/src/components/Footer/footer.js
@@ -1,41 +1,94 @@
 import React from "react";
 import Logo from "../../logo.png";
-import "./Footer.css";
 
 const footer = () => {
   return (
-    <footer>
-    <div style={bg}>
-      <div class="row">
-          <div class="col-md-6">
-            <div class="row">
-                <div class="col-md-6 ">
-                  <div style={{marginTop:'-20px'}}>
-                      <img style={im} src={Logo}/><br/><br/>
+    <footer class="pt-4">
+      <div style={bg}>
+        <div class="row">
+            <div class="col-md-3">
+              <div class="row">
+                    <div >
+                        <img className="" src={Logo}/>
                   </div>
-                </div>
+                  
+              </div>
+
             </div>
-          </div>
-          <div style={{marginLeft:'-40px'}} class="col-md-6">
-                <div style={{width:'550px', marginLeft:'-100px'}}>
-                  <div class="row ">
-                      <div style={{ marginTop:'20px',marginLeft:'-120px'}}>
-                      <p style={para} className="text-center">
-                        ‘Fitness Glory’ is a well-equipped gym managed by a well-qualified instructor with experienced trainers.
-                        Located in the heart of FITNESS Town, it is specially recommended for women looking for a decent place for 
-                        physical personal trainings, to maintain fitness and good health.
-                      </p>
-                      </div>
-                  </div>
-                </div>
-                <div style={navBtnSet}>
-                    <a href="#" style={navBtn1}> About Us </a><br/>
-                    <a href="#" style={navBtn2}> Contact Us </a><br/>
-                    <a href="/member/OtherQ/" style={navBtn3}> Q and A Section</a>
-                </div>
-          </div>
+            <div class="col-md-3">
+                    <div class="row m-md-3">
+                      <h6 class="text-light">FitnessGlory</h6>
+                        <hr/>
+                            <p className="text-muted">
+                              A well-equipped gym managed by a well-qualified instructor with experienced trainers.
+                            </p>
+                    </div>
+            </div>
+
+            <div class="col-md-3">
+                    <div class="row m-md-3">
+                      <h6 class="text-light">Contact us</h6>
+                        <hr/>
+                          <p class="text-muted">
+                            <i class="fas fa-home mr-3"></i> Gampaha, GM 102</p>
+                          <p class="text-muted">
+                            <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+                    </div>
+            </div>
+
+            <div class="col-md-3">
+                    <div class="row m-md-3">
+                      <h6 class="text-light">Useful links</h6>
+                        <hr/>
+                        <div className="text-muted">
+                          <a href="#"> About Us </a><br/>
+                          <a href="#"> Contact Us </a><br/>
+                          <a href="/member/OtherQ/"> Q and A Section</a>
+                        </div>
+                    </div>
+            </div>
+
+
+        </div>
+
+        <hr className="pt-1"/>
+        
+        <div class="row">
+            <div class="col-md-8">
+              <div class="container" >
+                © 2021 Copyright:
+                <a class="text-muted" href="#">FitnessGlory.com</a>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="">
+              <ul class="text-center">
+                    <li class="list-inline-item">
+                      <a class="btn btn-primary mx-2" style={{backgroundColor: "#3b5998"}}>
+                        <i class="fab fa-facebook"> </i>
+                      </a>
+                    </li>
+                    <li class="list-inline-item">
+                      <a class="btn btn-primary mx-2" style={{backgroundColor: "#55acee"}}>
+                        <i class="fab fa-twitter"> </i>
+                      </a>
+                    </li>
+                    <li class="list-inline-item">
+                      <a class="btn btn-primary mx-2" style={{backgroundColor: "#ac2bac"}}>
+                        <i class="fab fa-instagram"> </i>
+                      </a>
+                    </li>
+                    <li class="list-inline-item">
+                      <a class="btn btn-primary mx-2" style={{backgroundColor: "#c61118"}}>
+                        <i class="fab fa-pinterest"> </i>
+                      </a>
+                    </li>
+                  </ul>
+              </div>
+            </div>
+          
+        </div>
       </div>
-    </div>
 
     </footer>
   );
@@ -45,69 +98,62 @@ export default footer;
 
 const bg = {
   backgroundColor: "black",
-  opacity:'0.95',
-  padding:'10px'
 };
 
 const im = {
-  marginTop:'30px',
+
   height:'85%',
   width:'85%',
 };
 
-const navBtn3 = {
-  justifyContent: 'center',
-  marginLeft: '160px',
-  justifyContent: 'center',
-  marginLeft: '200px',
-  color: 'white',
-  textDecoration: 'none',
-  border: '2px solid',
-  fontSize: '13px',
-  padding: '10px 31px',
-  lineHeight: '47px',
-};
+// const navBtn3 = {
+//   justifyContent: 'center',
+//   marginLeft: '160px',
+//   justifyContent: 'center',
+//   marginLeft: '200px',
+//   color: 'white',
+//   textDecoration: 'none',
+//   border: '2px solid',
+//   fontSize: '13px',
+//   padding: '10px 31px',
+//   lineHeight: '47px',
+// };
 
-const navBtn2={
-  justifyContent: "center",
-  marginLeft: "190px",
-  justifyContent: "center",
-  marginLeft: "200px",
-  color: 'white',
-  textDecoration: 'none',
-  border: '2px solid',
-  fontSize: '13px',
-  padding: '9px 48px',
-  lineHeight: '47px',
-};
+// const navBtn2={
+//   justifyContent: "center",
+//   marginLeft: "190px",
+//   justifyContent: "center",
+//   marginLeft: "200px",
+//   color: 'white',
+//   textDecoration: 'none',
+//   border: '2px solid',
+//   fontSize: '13px',
+//   padding: '9px 48px',
+//   lineHeight: '47px',
+// };
 
-const navBtn1={
-  justifyContent: "center",
-  marginLeft: "200px",
-  color: 'white',
-  textDecoration: 'none',
-  border: '2px solid',
-  fontSize: '13px',
-  padding: '9px 55px',
-  lineHeight: '47px',
-}
+// const navBtn1={
+//   justifyContent: "center",
+//   marginLeft: "200px",
+//   color: 'white',
+//   textDecoration: 'none',
+//   border: '2px solid',
+//   fontSize: '13px',
+//   padding: '9px 55px',
+//   lineHeight: '47px',
+// }
 
 
-const txt  ={
-  marginLeft:'50px',
-  color:'white'
-}
+// const txt  ={
+//   marginLeft:'50px',
+//   color:'white'
+// }
 
-const heading ={
-  color:'white'
-}
+// const heading ={
+//   color:'white'
+// }
 
-const para ={
-  color:'white',
-  marginTop:'20px'
-}
-
-const navBtnSet ={
-  marginTop:'-120px',
-  marginLeft:'350px',
-}
+// const para ={
+//   color:'white',
+//   marginTop:'20px'
+// }
